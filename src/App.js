21663488943c40318123d2263bfc62ae45dea064
Shourya Singh Sengar <Shouryacode2021@gmail.com>
2023-08-20 02:10:06 +0530

    Important Note for API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
 const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=5baa7886";
-
+//USE https instead of http to make the API work
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
