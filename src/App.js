fdf8d19015bdfcd22707a4b0f724362acd14f093
Shourya Singh Sengar <Shouryacode2021@gmail.com>
2023-08-20 01:48:19 +0530

    Removing Error

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const API_URL = "http://www.omdbapi.com?apikey=5baa7886";
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
-    const [error, setError] = useState("");
+
 
     useEffect(() => {
         // setError("Please enter a search term.");
@@ -42,22 +42,16 @@ const App = () => {
             </div>
 
             {
-                error ? (
-                    <div className="empty">
-                        <h2>{error}</h2>
+                movies?.length > 0 ? (
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} key={movie.imdbID} />
+                        ))}
                     </div>
                 ) : (
-                    movies?.length > 0 ? (
-                        <div className='container'>
-                            {movies.map((movie) => (
-                                <MovieCard movie={movie} key={movie.imdbID} />
-                            ))}
-                        </div>
-                    ) : (
-                        <div className="empty">
-                            <h2>No movies found</h2>
-                        </div>
-                    )
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
                 )
             }
         </div>
